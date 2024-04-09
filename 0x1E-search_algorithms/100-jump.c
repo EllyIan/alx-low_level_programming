f5862b74ae6c4aefa844b49f8aa39535f2c2ff79
EllyIan <ianmongi45@gmail.com>
2024-04-09 06:36:06 -0700

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,14 +1,11 @@
 #include "search_algos.h"
 
 /**
- * jump_search - Performs a jump search on a sorted array.
- *
- * @array: Pointer to the first element of the array to search.
- * @size: Number of elements in the array.
- * @value: The value to search for.
- *
- * Return: The index of the first occurrence of the value, 
- *         or -1 if not found.
+ * jump_search - searches for a vulue in a sorted array
+ * @array: pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: value to search for
+ * Return: the first index of the value in the array
  */
 
 int jump_search(int *array, size_t size, int value)
@@ -18,24 +15,21 @@ int jump_search(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 
-	/* Calculate jump step size (assuming a pre-sorted array) */
+	/* Handle invalid input */
 	jump_step = sqrt(size);
-
-	/* Traverse the array with jumps */
-	for (i = prev_index = 0; prev_index < size && array[prev_index] < value;) {
+	for (i = prev_index = 0; prev_index < size && array[prev_index] < value;)
+	{
 		printf("Value checked array[%ld] = [%d]\n", prev_index, array[prev_index]);
 		i = prev_index;
 		prev_index += jump_step;
 	}
 
-	/* Narrow down search range */
-	prev_index = prev_index < size - 1 ? prev_index : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i, prev_index);
 
-	/* Linear search within narrowed range */
+	prev_index = prev_index < size - 1 ? prev_index : size - 1;
 	for (; i < prev_index && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 	return (array[i] == value ? (int)i : -1);
 }
-
